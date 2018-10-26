fdf19a5aff3ff8339cb128592135ae54184bd0b5
kovinevmv <kovinevmv@gmail.com>
2018-10-26 03:22:50 +0300

    #2 Add Espresso tests
    - Добавлены тесты
    - Обновлен gradle

--- app/src/androidTest/java/com/leti/progin6304/cameraphotographerassistant/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/leti/progin6304/cameraphotographerassistant/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.leti.progin6304.cameraphotographerassistant
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,21 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule
+    @JvmField
+    var mActivityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.leti.progin6304.cameraphotographerassistant", appContext.packageName)
+    fun isVisibleCamera() {
+        onView(withId(R.id.camera_preview)).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun isClickableButton() {
+        onView(withId(R.id.shutter)).perform(click())
+                .check(matches(isDisplayed()))
+    }
+
+
+
 }
